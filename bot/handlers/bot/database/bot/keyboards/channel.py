d084fa340f3b91f957c372a427a5941fb7bedcc3
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 11:13:29 +0330

    Create channel.py

---
+++ bot/handlers/bot/database/bot/keyboards/channel.py
@@ -0,0 +1,18 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+channel_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="📢 عضویت در کانال",
+                url="https://t.me/mohrehmarradobargh"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✅ بررسی عضویت",
+                callback_data="check_join"
+            )
+        ]
+    ]
