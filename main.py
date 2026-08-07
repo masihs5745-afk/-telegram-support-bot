8965b6836d9a33b3def112428e71ecc7f0b78293
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 09:44:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from bot.handlers.order import router as order_router
 from bot.handlers.support import router as support_router
 from bot.handlers.admin import router as admin_router
 from bot.handlers.admin_panel import router as admin_panel_router
+from bot.handlers.info import router as info_router
 from bot.database.database import create_db
 
 
@@ -19,6 +20,7 @@ dp.include_router(order_router)
 dp.include_router(support_router)
 dp.include_router(admin_router)
 dp.include_router(admin_panel_router)
+dp.include_router(info_router)
 
 
 async def main():
