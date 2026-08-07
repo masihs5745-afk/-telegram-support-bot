dfc81ee35e9185b0fcd548b4a3513bbc33098abc
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 09:29:30 +0330

    Update support.py

--- bot/handlers/bot/database/support.py
+++ bot/handlers/bot/database/support.py
@@ -26,3 +26,19 @@ async def save_support_message(
         )
 
         await db.commit()
+async def get_user_by_message(message_id: int):
+    async with aiosqlite.connect(DB_NAME) as db:
+        cursor = await db.execute(
+            """
+            SELECT user_id FROM support_messages
+            WHERE message_id = ?
+            """,
+            (message_id,)
+        )
+
+        result = await cursor.fetchone()
+
+        if result:
+            return result[0]
+
+        return None
