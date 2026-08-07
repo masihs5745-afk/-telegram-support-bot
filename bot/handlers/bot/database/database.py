50ebf412a3dd3a1cd9ab96b4be6b077d50d43d1f
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 08:41:56 +0330

    Create database.py

---
+++ bot/handlers/bot/database/database.py
@@ -0,0 +1,29 @@
+import aiosqlite
+
+
+DB_NAME = "bot.db"
+
+
+async def create_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            user_id INTEGER UNIQUE,
+            full_name TEXT,
+            username TEXT
+        )
+        """)
+        await db.commit()
+
+
+async def add_user(user_id: int, full_name: str, username: str):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            """
+            INSERT OR IGNORE INTO users (user_id, full_name, username)
+            VALUES (?, ?, ?)
+            """,
+            (user_id, full_name, username)
+        )
+        await db.commit()
