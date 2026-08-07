ea588e7845a3f46ff5c6330902513db1200a4935
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 08:38:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from bot.config import BOT_TOKEN
+from bot.handlers.start import router as start_router
 
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+dp.include_router(start_router)
 
 
 async def main():
