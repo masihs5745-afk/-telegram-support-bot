0eb3f0cc8496fba84949010aec6119344c8cd085
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 09:37:04 +0330

    Update admin_panel.py

--- bot/handlers/bot/admin_panel.py
+++ bot/handlers/bot/admin_panel.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 from bot.config import ADMIN_ID
+from bot.database.database import get_users_count, get_orders_count
 
 
 router = Router()
@@ -13,8 +14,12 @@ async def admin_panel(message: Message):
     if message.from_user.id != ADMIN_ID:
         return
 
+    users = await get_users_count()
+    orders = await get_orders_count()
+
     await message.answer(
         "👨‍💻 پنل مدیریت\n\n"
-        "📊 آمار\n"
-        "📢 ارسال همگانی"
+        f"👥 تعداد کاربران: {users}\n"
+        f"📦 تعداد سفارش‌ها: {orders}\n\n"
+        "📢 ارسال همگانی به‌زودی اضافه می‌شود."
     )
