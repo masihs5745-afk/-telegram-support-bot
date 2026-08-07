2972ae78752c3bcdb54ce48c9c7f308a0f1270ef
masihs5745-afk <masihs5745@gmail.com>
2026-08-07 11:14:26 +0330

    Create main_menu.py

---
+++ bot/handlers/bot/database/bot/keyboards/main_menu.py
@@ -0,0 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="🛒 ثبت سفارش"),
+            KeyboardButton(text="📞 پشتیبانی")
+        ],
+        [
+            KeyboardButton(text="ℹ️ درباره ما"),
+            KeyboardButton(text="📋 قوانین")
+        ]
+    ],
+    resize_keyboard=True
+)
